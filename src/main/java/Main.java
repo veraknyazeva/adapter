8f3f00bcbfd1d086ec2b50b1685b606acb54b0cf
Vera Knyazeva <verashibanova@yandex.ru>
2024-07-01 12:12:56 +0500

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(2, 2));
+        System.out.println(calc.sum(10, 22));
+        System.out.println(calc.pow(2, 10));
+        System.out.println(calc.mult(5,5));
+    }
+}
